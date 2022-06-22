27de03477ea3c3d255e9769d031c65a86b410c3b
Benjamin Kenesei <beni1998@hotmail.hu>
2022-06-22 09:03:41 +0200

    v1.0

---
+++ magic_resizer.py
@@ -0,0 +1,60 @@
+from PIL import Image
+import os
+
+
+def get_file_size_inMB(file):
+    return str(os.stat(file).st_size / 1000000)
+
+
+def get_folder_size(root):
+    size = 0
+    for path, dirs, files in os.walk(root):
+        for f in files:
+            fp = os.path.join(path, f)
+            size += os.path.getsize(fp)
+    return round(size / 1000000, 2)
+
+
+path = "E:/test/"
+original_folder_size = get_folder_size(path)
+
+path = input("Specify the root dir\nLike C:/dir/pics/\nRoot dir: ")
+custom_quality = int(input("Quality: "))
+min_file_size = input("Min file size to convert(in MB): ")
+
+files_fullpath = []
+counter = 0
+for subdir, dirs, files in os.walk(path):
+    for file in files:
+        if get_file_size_inMB(subdir + '/' + file) > min_file_size:
+            files_fullpath.append(subdir + '/' + file)
+
+print("Files found: " + str(len(files_fullpath)))
+
+
+def resize():
+    percent = 0
+    i = 0
+    for file in files_fullpath:
+        if percent + 1 < round(100 * i / len(files_fullpath), 2):
+            percent = round(100 * i / len(files_fullpath), 2)
+            print(str(percent) + " % done")
+        if os.path.isfile(file) and file.split('.')[-1].lower() == 'jpg':
+            im = Image.open(file)
+            f, e = os.path.splitext(file)
+            im.save(f + '.jpg', 'JPEG', quality=custom_quality)
+            i = i + 1
+        if os.path.isfile(file) and file.split('.')[-1].lower() == 'bmp':
+            im = Image.open(file)
+            f, e = os.path.splitext(file)
+            im.save(f + '.jpg', 'JPEG', quality=custom_quality)
+            im.close()
+            os.remove(file)
+            i = i + 1
+
+
+resize()
+
+resized_folder_size = get_folder_size(path)
+print("Original total size: " + str(original_folder_size) + " MB")
+print("Resized  total size: " + str(resized_folder_size) + " MB")
